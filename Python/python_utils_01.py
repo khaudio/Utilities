c30f66ceb3884de64d27f1d4381273be41939b42
Kyle Hughes <kylehughes1@gmail.com>
2018-02-27 20:31:04 -0600

    organized some things into classes

--- Python/python_utils_01.py
+++ Python/python_utils_01.py
@@ -6,64 +6,134 @@ Code stored for re-use
 
 from threading import Thread
 from multiprocessing import Queue
+from serial import Serial, EIGHTBITS
+import asyncio
 
 
-# Multiple operations in one lambda
-lambda x: (x * 1, x + 3, x - 6)
+# Create and start a function and thread with multiple operations
+x = 1
+t = Thread(target=lambda: (x * 1, x + 3, x - 6)).start()
 
 
-# Create a and start a function and thread in one line
-Thread(target=lambda x: x + 1).start()
+class CommBase(object):
+    def __init__(self, port='/dev/ttyACM0', baud=9600):
+        self.alive, self.preamble, self.escape, self.received = True, '\x3C', '\x0A', Queue()
+        self.serial = Serial(port=port, baudrate=baudrate, bytesize=EIGHTBITS, timeout=1)
+        self.reader = Process(target=self.read_from_serial)
+        self.listener = Process(target=self.listen)
+        for proc in [self.reader, self.listener]:
+            proc.start()
 
 
-def listen():
-    """Process serial messages"""
-    while True:
-        for message in read_from_serial():
-            payload = b''.join(data).decode('ascii')
+    def __enter__(self):
+        return self
+
+
+    def __exit__(self, a, b, c):
+        self.alive = False
+        map(lambda proc: (proc.terminate(), proc.join()), (self.reader, self.listener))
+        self.serial.close()
+
+
+    def run(self):
+        while True:
+            try:
+                pass
+            except KeyboardInterrupt:
+                break
+            sleep(.0001)
+
+
+    def listen(self):
+        """Process serial messages"""
+        while self.alive:
+            message = self.received.get()
+            payload = b''.join(message).decode('ascii')
             print(payload)
-        sleep(.0001)
+            del message, payload
 
 
-def read_from_serial(q=None, preamble='\x3C', escape='\x0A'):
-    """Read messages from a serial device"""
-    message, incoming = [], False
-    while True:
-        data = serial.read()
-        if data:
-            if not incoming:
-                if data == preamble:
-                    incoming = True
-                    message = []
-            elif incoming:
-                if data == escape:
-                    yield message
-                    if q:
-                        q.put(message)
-                    incoming = False
-                else:
-                    message.append(data)
-        del data
-        sleep(.0001)
-    serial.close()
+    def read_from_serial(self):
+        """Read messages from a serial device"""
+        message, incoming = [], False
+        while self.alive:
+            data = self.serial.read()
+            if data:
+                if not incoming:
+                    if data == self.preamble:
+                        incoming = True
+                        message = []
+                elif incoming:
+                    if data == self.escape:
+                        yield message
+                        self.received.put(message)
+                        incoming = False
+                    else:
+                        message.append(data)
+            del data
 
 
-def write_to_serial(command, length=0, preamble='\x3C', escape='\x0A'):
-    """Write messages to a serial device"""
-    transmission = b''
-    if isinstance(command, list) and length:
-        if len(command) == length:
-            transmission = bytearray(command)
+    def write_to_serial(self, command, length=0):
+        """Write messages to a serial device"""
+        transmission = b''
+        if isinstance(command, list) and length:
+            if len(command) == length:
+                transmission = bytearray(command)
+            else:
+                raise ValueError(f'Command length must be {length}')
+        elif isinstance(command, str):
+            transmission = bytes(command.encode('utf-8'))
+        elif isinstance(command, bytes):
+            self.serial.write(command)
         else:
-            raise ValueError(f'Command length must be {length}')
-    elif isinstance(command, str):
-        transmission = bytes(command.encode('utf-8'))
-    elif isinstance(command, bytes):
-        serial.write(command)
-    else:
-        raise TypeError('Command must be of type list, string, or bytes')
-    if transmission:
-        serial.write(preamble + transmission + escape)
+            raise TypeError('Command must be of type list, string, or bytes')
+        if transmission:
+            self.serial.write(self.preamble + transmission + self.escape)
+
+
+class ExampleAsync(object):
+    def __init__(self):
+        self.loop = asyncio.get_event_loop()
+        self.places = set()
+
+
+    def __enter__(self):
+        return self
+
+
+    def __exit__(self, a, b, c):
+        self.loop.close()
+
+
+    def add(self, *places):
+        for place in places:
+            assert isinstance(place, str), 'Must be str'
+            self.places.add(place)
+
+
+    def start(self):
+        """Start the event loop to run the coroutines"""
+        coroutines = []
+        for place in self.places:
+            coroutines.extend([
+                asyncio.ensure_future(self.hello(place)),
+                asyncio.ensure_future(self.goodbye(place))
+                ])
+        self.loop.run_until_complete(asyncio.gather(*coroutines))
+
+
+    async def hello(self, place):
+        """Example async coroutine"""
+        print('Hello')
+        await asyncio.sleep(.4)
+        print(place)
+
+
+    async def goodbye(self, place):
+        """Example async coroutine"""
+        print('Goodbye')
+        await asyncio.sleep(1.3)
+        print(place)
 
 
 def round_down(amount, decimalPlaces=0):
@@ -77,12 +147,8 @@ def special_text(text, effect='grey', percentage=False):
     Adds effects to print output.
     Positive numbers are green, negative numbers are red, unless overridden.
     """
-    effects = {'red': '\033[91m',
-               'grey': '\033[0m',
-               'green': '\033[92m',
-               'yellow': '\033[93m',
-               'purple': '\033[95m',
-               'bold': '\033[1m'}
+    effects = {'red': '\033[91m', 'grey': '\033[0m', 'green': '\033[92m',
+               'yellow': '\033[93m', 'purple': '\033[95m', 'bold': '\033[1m'}
     if isinstance(text, (int, float)) and effect is 'grey':
         if text > 0:
             effect = 'green'
@@ -91,7 +157,7 @@ def special_text(text, effect='grey', percentage=False):
         text = str(text)
         if percentage:
             text = text + ' %'
-    return effects[effect] + text + effects['grey']
+    return ''.join(effects[effect], text, effects['grey'])
 
 
 def random_data(x, percentChange):
@@ -105,36 +171,8 @@ def random_data(x, percentChange):
             yield (shift * (-1))
 
 
-# Tuple of places to asynchronously iterate over
-places = ('home', 'valhalla', 'world')
-
-
-# Example async coroutine
-async def hello(place):
-    print('Hello')
-    await asyncio.sleep(.4)
-    print(place)
-
-
-# Example async coroutine
-async def goodbye(place):
-    print('Goodbye')
-    await asyncio.sleep(1.3)
-    print(place)
-
-
-# Add coroutines to the loop
-for place in places:
-    asyncio.ensure_future(hello(place))
-    asyncio.ensure_future(goodbye(place))
-
-
-# asyncio loop example/boilerplate
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        loop.close()
+    places = ('home', 'valhalla', 'world')
+    with ExampleAsync() as e:
+        e.add(*places)
+        e.start()
